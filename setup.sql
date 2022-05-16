9771e66f144655ac395576e2eeb8388a0e137aeb
Bryce Cordill <brycecordill@gmail.com>
2022-05-15 18:41:35 -0700

    Reorganized sql files

--- setup.sql
+++ setup.sql
@@ -4,9 +4,3 @@ create table Logins(
     pHash varbinary(32) not null,
     pSalt varchar(64) not null
 );
-
-insert into Logins(uname, pHash, pSalt) values('test1',
-	0x7a37b85c8918eac19a9089c0fa5a2ab4dce3f90528dcdeec108b23ddf3607b99,
-    'salt');
-    
-SELECT pHash, pSalt FROM Logins WHERE uname='test1';
---
+++ testInput.sql
@@ -0,0 +1,5 @@
+insert into Logins(uname, pHash, pSalt) values('test1',
+	0x7a37b85c8918eac19a9089c0fa5a2ab4dce3f90528dcdeec108b23ddf3607b99,
+    'salt');
+    
+SELECT pHash, pSalt FROM Logins WHERE uname='test1';
